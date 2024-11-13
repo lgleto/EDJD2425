cde59801d5f0341951e65d3a8a75d4954688346e
Lourenço <leto.lg@gmail.com>
2024-11-13 12:04:51 +0000

    Shopping List

---
+++ ShoppingList/app/src/main/java/ipca/example/shoppinglist/MainActivity.kt
@@ -0,0 +1,33 @@
+package ipca.example.shoppinglist
+
+import android.os.Bundle
+import androidx.activity.ComponentActivity
+import androidx.activity.compose.setContent
+import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import ipca.example.shoppinglist.ui.theme.ShoppingListTheme
+
+const val TAG = "ShoppingList"
+
+class MainActivity : ComponentActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContent {
+            ShoppingListTheme {
+                Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
+                    LoginView(
+                        modifier = Modifier.padding(innerPadding)
+                    )
+                }
+            }
+        }
+    }
+}
+
